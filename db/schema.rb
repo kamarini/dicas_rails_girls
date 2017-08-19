66e5d0f0d1f1aef622b7352eb663b0d8516c9c24
carolmollo <lizzy.maddog@gmail.com>
2017-08-19 16:49:43 -0300

    Add Comments controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170819171147) do
+ActiveRecord::Schema.define(version: 20170819194015) do
 
   create_table "comments", force: :cascade do |t|
     t.string "author"
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "post_id"
+    t.index ["post_id"], name: "index_comments_on_post_id"
   end
 
   create_table "posts", force: :cascade do |t|
